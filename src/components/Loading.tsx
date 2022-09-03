3eb10be5445d13bc3871e91fc871c28e406785c9
Theofilos Chamalis <theofxam@gmail.com>
2022-09-03 11:40:36 +0300

    Made the loading svg compatible with Safari based browsers

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,24 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import Image from "next/image";
 
 const Loading: FunctionComponent = () => {
+  const [usesWebkit, setUsesWebkit] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    setUsesWebkit(
+      navigator.userAgent.indexOf("Safari") > -1 &&
+        navigator.userAgent.indexOf("Chrome") <= -1
+    );
+  }, []);
+
   return (
-    <div className="flex h-screen w-full justify-center -mt-32">
-      <Image src={"/loading-animated.svg"} width={240} height={240} />
+    <div className="flex h-screen w-full justify-center items-center -mt-32">
+      {usesWebkit === false && (
+        <Image src={"/loading-animated.svg"} width={240} height={240} />
+      )}
+      {usesWebkit && (
+        <object data={"/loading-animated.svg"} width={240} height={240} />
+      )}
     </div>
   );
 };
